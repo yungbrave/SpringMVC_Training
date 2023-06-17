f844702fd6cfbb8edeba4c9c54c6d1c4a36621ef
yungbrave <SergeyIlinR@gmail.com>
2023-06-17 04:33:10 +0300

    initial commit

---
+++ src/main/java/com/yungbrave/spring/mvc/MyController.java
@@ -0,0 +1,13 @@
+package com.yungbrave.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "firstView";
+    }
+}
